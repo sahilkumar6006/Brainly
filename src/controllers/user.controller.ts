a716415682eb1c08afb966d9d614e4cdfa91a8f0
M2 <sahil.kumar@geeky.dev>
2025-03-10 11:58:08 +0530

    initial bootsrapping

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,12 @@
 
 const Signin = (req: Request, res: Response) => {
     const {username, password} = req.body;
-    
+    if(!username || !password){
+        res.status(200, {
+            message: 'provide the username and password'
+        })
+    }
+
 }
 
 
--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,3 @@ const app = express();
 
 ConnectDb();
 
-
-app.post("/api/v1/signup", (req, res) => {
-
-})
-app.get("/api/v1/content", (req, res) => {
-
-})
-app.delete("/api/v1/content", (req, res) => {
-
-})
-
-app.post("/api/v1/brain/share", (req, res) => {
-
-})
-
-app.post("/api/v1/brain/:shareLink", (req, res) => {
-
-})
